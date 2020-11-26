425dd77335c66a5c91f52612cc09fdc3862db97d
a6hinavram <a6hinavram@gmail.com>
2020-11-26 11:16:14 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,40 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
 //import { HttpClient } from '@angular/common/http';
-import { AgGridAngular } from 'ag-grid-angular';
+import { AgGridAngular } from "ag-grid-angular";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
-    @ViewChild('agGrid') agGrid: AgGridAngular;
+  @ViewChild("agGrid") agGrid: AgGridAngular;
 
-    title = 'my-app';
+  title = "my-app";
 
-    columnDefs = [
-        { field: 'make', sortable: true, filter: true, checkboxSelection: true },
-        { field: 'model', sortable: true, filter: true },
-        { field: 'price', sortable: true, filter: true }
-    ];
-
-    rowData: any;
-
-    // constructor(private http: HttpClient) {
-
-    // }
-
-    ngOnInit() {
-        // this.rowData = this.http.get('https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/sample-data/smallRowData.json');
-
-         this.rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 }
+  columnDefs = [
+    { field: "make", sortable: true, filter: true, checkboxSelection: true },
+    { field: "model", sortable: true, filter: true },
+    { field: "price", sortable: true, filter: true }
   ];
-    }
 
-    getSelectedRows() {
-        const selectedNodes = this.agGrid.api.getSelectedNodes();
-        const selectedData = selectedNodes.map(node => node.data );
-        const selectedDataStringPresentation = selectedData.map(node => node.make + ' ' + node.model).join(', ');
+  rowData: any;
 
-        alert(`Selected nodes: ${selectedDataStringPresentation}`);
-    }
+  ngOnInit() {
+    this.rowData = [
+      { make: "Toyota", model: "Celica", price: 35000 },
+      { make: "Ford", model: "Mondeo", price: 32000 },
+      { make: "Porsche", model: "Boxter", price: 72000 }
+    ];
+  }
+
+  getSelectedRows() {
+    const selectedNodes = this.agGrid.api.getSelectedNodes();
+    const selectedData = selectedNodes.map(node => node.data);
+    const selectedDataStringPresentation = selectedData
+      .map(node => node.make + " " + node.model)
+      .join(", ");
+
+    alert(`Selected nodes: ${selectedDataStringPresentation}`);
+  }
 }
