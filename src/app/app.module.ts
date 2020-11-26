cc89a9e1fe36dad2d814ebffe700769665f589a1
a6hinavram <a6hinavram@gmail.com>
2020-11-26 11:54:22 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {NumberFormatterComponent} from './number-formatter.component';
   ],
   declarations: [
     AppComponent,
+    NumberFormatterComponent
 
   ],
   bootstrap: [ AppComponent ]
