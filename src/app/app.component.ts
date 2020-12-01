cf1232a7fdf49ea42067701506af2b3fe71ed700
a6hinavram <a6hinavram@gmail.com>
2020-12-01 10:37:49 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
-import {GridOptions} from "ag-grid";
-import { HttpClient } from '@angular/common/http';
+import { Component } from "@angular/core";
+import { GridOptions } from "ag-grid";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "app-root",
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
-
   private gridApi;
   private gridColumnApi;
 
@@ -15,40 +14,38 @@ export class AppComponent {
   private defaultColDef;
   private rowData: [];
 
-    private gridOptions: GridOptions;
+  private gridOptions: GridOptions;
 
-    constructor(private http: HttpClient) {
-        this.gridOptions = <GridOptions>{
-          enableSorting: true,
-          // enable filtering 
-          enableFilter: true
-        };
-       
-        // Column Defs
-        this.gridOptions.columnDefs = [
-        {headerName: '', field: ''},
-        {headerName: 'JobID', field: 'JobID'},
-        {headerName: 'Date', field: 'CreatedDate'},
-        {headerName: 'Title', field: 'Title'},
-        {headerName: 'Company', field: 'CompanyName'},
-        {headerName: 'Hiring Manager', field: ''},
-        {headerName: 'Location', field: 'Location'},
-        {headerName: 'Status', field: 'JobStatus'},
+  constructor(private http: HttpClient) {
+    this.gridOptions = <GridOptions>{
+      enableSorting: true,
+      // enable filtering
+      enableFilter: true
+    };
+
+    // Column Defs
+    this.gridOptions.columnDefs = [
+      { headerName: "", field: "" },
+      { headerName: "JobID", field: "JobID" },
+      { headerName: "Date", field: "CreatedDate" },
+      { headerName: "Title", field: "Title" },
+      { headerName: "Company", field: "CompanyName" },
+      { headerName: "Hiring Manager", field: "" },
+      { headerName: "Location", field: "Location" },
+      { headerName: "Status", field: "JobStatus" }
     ];
-    }
+  }
 
-
-    onGridReady(params) {
+  onGridReady(params) {
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
 
     this.http
       .get(
-        'https://hiringmanagerwebapi.azurewebsites.net/api/job/GetAllJobsInfo'
+        "https://hiringmanagerwebapi.azurewebsites.net/api/job/GetAllJobsInfo"
       )
-      .subscribe((data) => {
+      .subscribe(data => {
         this.rowData = data;
       });
   }
-
 }
