2cd683791cb8c7c583dd43849842903052257212
a6hinavram <a6hinavram@gmail.com>
2020-12-02 18:47:05 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,9 @@ import { HttpClient } from "@angular/common/http";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-   host: {
-        "(window:resize)":"onWindowResize($event)"
-    }
+  host: {
+    "(window:resize)": "onWindowResize($event)"
+  }
 })
 export class AppComponent {
   private gridApi;
@@ -17,63 +17,47 @@ export class AppComponent {
   private defaultColDef;
   private rowData: [];
 
-  public mobileColumn = [{ headerName: "JobID", field: "JobID" },
-      { headerName: "Title", field: "Title" },
-      { headerName: "Company", field: "CompanyName" },
-      { headerName: "Status", field: "JobStatus" }];
+  public mobileColumn = [
+    { headerName: "JobID", field: "JobID" },
+    { headerName: "Title", field: "Title" },
+    { headerName: "Company", field: "CompanyName" },
+    { headerName: "Status", field: "JobStatus" }
+  ];
 
   private gridOptions: GridOptions;
   browserWidth: number = window.innerWidth;
-    browserHeight: number = window.innerHeight;
+  browserHeight: number = window.innerHeight;
 
   constructor(private http: HttpClient) {
     this.gridOptions = <GridOptions>{
       enableSorting: true,
-      // enable filtering
-      enableFilter: true
+        enableFilter: true
     };
 
     // Column Defs
-    
-
-  this.gridOptions.columnDefs = [
-       { headerName: 'Make', field: 'make' },
-  { headerName: 'Model', field: 'model' },
-  { headerName: 'Price', field: 'price' }
+    this.gridOptions.columnDefs = [
+      { headerName: "Make", field: "make" },
+      { headerName: "Model", field: "model" },
+      { headerName: "Price", field: "price" }
     ];
 
-    
-
-   this.gridOptions.pagination = true;
-
-   this.gridOptions.skipHeaderOnAutoSize = true;
-
-    
+    this.gridOptions.pagination = true;
+    this.gridOptions.skipHeaderOnAutoSize = true;
   }
 
   sizeToFit() {
     this.gridApi.sizeColumnsToFit();
   }
 
-  autoSizeAll(skipHeader) {
-    var allColumnIds = [];
-    this.gridColumnApi.getAllColumns().forEach(function (column) {
-      allColumnIds.push(column.colId);
-    });
-    this.gridColumnApi.autoSizeColumns(allColumnIds, skipHeader);
-  }
-
-
   onGridReady(params) {
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
-
     this.sizeToFit();
 
     this.rowData = [
-  { make: 'Toyota', model: 'Celica', price: 35000 },
-  { make: 'Ford', model: 'Mondeo', price: 32000 },
-  { make: 'Porsche', model: 'Boxter', price: 72000 }
-];
+      { make: "Toyota", model: "Celica", price: 35000 },
+      { make: "Ford", model: "Mondeo", price: 32000 },
+      { make: "Porsche", model: "Boxter", price: 72000 }
+    ];
   }
 }
