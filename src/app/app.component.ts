a93f8dc3924cd0399804132de79c6c11a7c56cfe
a6hinavram <a6hinavram@gmail.com>
2020-12-04 19:31:47 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,33 @@
-import { Component } from '@angular/core';
-import {GridOptions} from "ag-grid";
+import { Component } from "@angular/core";
+import { GridOptions } from "ag-grid";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "app-root",
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
+  private gridOptions: GridOptions;
+  quickSearchValue: string = "";
 
-    private gridOptions: GridOptions;
-    quickSearchValue: string = '';
+  constructor() {
+    this.gridOptions = <GridOptions>{
+      enableSorting: true,
+      // enable filtering
+      enableFilter: true
+    };
+    this.gridOptions.columnDefs = [
+      { headerName: "Make", field: "make", resizable: true },
+      { headerName: "Model", field: "model" },
+      { headerName: "Price", field: "price" }
+    ];
+    this.gridOptions.rowData = [
+      { make: "Toyota", model: "Celica", price: 35000 },
+      { make: "Ford", model: "Mondeo", price: 32000 },
+      { make: "Porsche", model: "Boxter", price: 72000 }
+    ];
+  }
 
-    constructor() {
-        this.gridOptions = <GridOptions>{
-          enableSorting: true,
-          // enable filtering 
-          enableFilter: true
-        };
-        this.gridOptions.columnDefs = [
-           {headerName: 'Make', field: 'make', resizable: true},
-        {headerName: 'Model', field: 'model'},
-        {headerName: 'Price', field: 'price'}
-
-        ];
-        this.gridOptions.rowData = [
-             {make: 'Toyota', model: 'Celica', price: 35000},
-        {make: 'Ford', model: 'Mondeo', price: 32000},
-        {make: 'Porsche', model: 'Boxter', price: 72000}
-        ]
-    }
-
-    onQuickFilterChanged() {
+  onQuickFilterChanged() {
     this.gridOptions.api.setQuickFilter(this.quickSearchValue);
-}
-
+  }
 }
