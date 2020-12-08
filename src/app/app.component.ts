66ff70ba5771d53886a86058067a567cf96810cc
a6hinavram <a6hinavram@gmail.com>
2020-12-08 11:59:41 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {GridOptions} from "ag-grid";
+import { GridOptions } from "ag-grid";
 
 @Component({
   selector: 'app-root',
@@ -7,25 +7,30 @@ import {GridOptions} from "ag-grid";
 })
 export class AppComponent {
 
-    private gridOptions: GridOptions;
+  private gridOptions: GridOptions;
 
-    constructor() {
-        this.gridOptions = <GridOptions>{
-          enableSorting: true,
-          // enable filtering 
-          enableFilter: true
-        };
-        this.gridOptions.columnDefs = [
-           {headerName: 'Make', field: 'make', resizable: true},
-        {headerName: 'Model', field: 'model'},
-        {headerName: 'Price', field: 'price'}
-
-        ];
-        this.gridOptions.rowData = [
-             {make: 'Toyota', model: 'Celica', price: 35000},
-        {make: 'Ford', model: 'Mondeo', price: 32000},
-        {make: 'Porsche', model: 'Boxter', price: 72000}
+  constructor() {
+    this.gridOptions = <GridOptions>{
+      enableSorting: true,
+      // enable filtering 
+      enableFilter: true
+    };
+    this.gridOptions.columnDefs = [
+      {
+        headerName: 'Group A',
+        children: [
+          { headerName: 'Make', field: 'make', resizable: true },
+          { headerName: 'Model', field: 'model' },
         ]
-    }
+      },
+      { headerName: 'Price', field: 'price' },
+
+    ];
+    this.gridOptions.rowData = [
+      { make: 'Toyota', model: 'Celica', price: 35000 },
+      { make: 'Ford', model: 'Mondeo', price: 32000 },
+      { make: 'Porsche', model: 'Boxter', price: 72000 }
+    ]
+  }
 
 }
