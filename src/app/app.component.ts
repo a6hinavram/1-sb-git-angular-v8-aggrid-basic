1f8079dc4479853a98bb4191cb1cb74f7e26f37a
a6hinavram <a6hinavram@gmail.com>
2020-12-10 11:49:14 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,36 @@
-import { Component } from '@angular/core';
-import {NumberFormatterComponent} from './number-formatter.component';
+import { Component } from "@angular/core";
+import { NumberFormatterComponent } from "./number-formatter.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "app-root",
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
-
   columnDefs = [
-        {headerName: 'Make', field: 'make'},
-        {headerName: 'Model', field: 'model'},
-        {
-          headerName: 'Price', 
-          field: 'price',
-          cellRenderer: 'numberFormatterComponent',
-          }
-    ];
+    { headerName: "Make", field: "make" },
+    { headerName: "Model", field: "model" },
+    {
+      headerName: "Price",
+      field: "price",
+      cellRenderer: "numberFormatterComponent"
+    },
+    {
+      headerName: "Pricee",
+      field: "price",
+      valueGetter: function(params) {
+        return params.data.price + 1;
+      },
+      cellRenderer: "numberFormatterComponent"
+    }
+  ];
 
-    frameworkComponents = {
-    numberFormatterComponent: NumberFormatterComponent,
-
+  frameworkComponents = {
+    numberFormatterComponent: NumberFormatterComponent
   };
 
-    rowData = [
-        {make: 'Toyota', model: 'Celica', price: 35000},
-        {make: 'Ford', model: 'Mondeo', price: 32000},
-        {make: 'Porsche', model: 'Boxter', price: 72000}
-    ];
-
+  rowData = [
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxter", price: 72000 }
+  ];
 }
